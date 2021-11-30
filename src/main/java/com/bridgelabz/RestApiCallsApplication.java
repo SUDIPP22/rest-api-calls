c86a3798175f90348125ef7831098a9aa518957c
Sudip Panja <panja370@gmail.com>
2021-12-01 04:42:15 +0530

    Added : Spring boot application for RestApiCallsApplication class

---
+++ src/main/java/com/bridgelabz/RestApiCallsApplication.java
@@ -0,0 +1,20 @@
+package com.bridgelabz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Purpose : To simulate REST Controller API
+ *
+ * @author SUDIP PANJA
+ * @version : 0.0.1-SNAPSHOT
+ * @since 2021-12-01
+ */
+@SpringBootApplication
+public class RestApiCallsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestApiCallsApplication.class, args);
+    }
+
+}
