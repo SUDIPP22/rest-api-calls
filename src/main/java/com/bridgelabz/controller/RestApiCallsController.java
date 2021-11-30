1a450b419b30242feb221185c0885bf9392ae251
Sudip Panja <panja370@gmail.com>
2021-12-01 05:11:38 +0530

    Added : Code to display message using GET request method and passing name as query parameter

--- src/main/java/com/bridgelabz/controller/RestApiCallsController.java
+++ src/main/java/com/bridgelabz/controller/RestApiCallsController.java
@@ -1,6 +1,7 @@
 package com.bridgelabz.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -17,5 +18,12 @@ public class RestApiCallsController {
     @GetMapping(value = "/message")
     public String getMessage() {
         return "Hello From BridgeLabz";
+    }
+
+    @GetMapping(value = "/message2")
+    public String getMessageAsQueryParameter
+            (@RequestParam String firstName,
+             @RequestParam String lastName) {
+        return "Hello From BridgeLabz : " + firstName + " " + lastName;
     }
 }
